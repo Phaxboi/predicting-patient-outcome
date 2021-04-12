823ac99a92cdf5cd8f2d0e5bfea184fc725c8e75
Philip <philip.s_08@hotmail.com>
2021-04-12 16:00:03 +0200

    fliter on ICU stays and transfers

--- mimic4csv.py
+++ mimic4csv.py
@@ -50,3 +50,16 @@ def remove_icustays_with_transfers(stays):
     return stays[['subject_id', 'hadm_id', 'stay_id', 'first_careunit', 'last_careunit' 'intime', 'outtime', 'los']]
 
 
+#filter out admissions which have more(or less) than one ICU stay per admission
+def filter_admissions_on_nb_icustays(stays, min_nb_stays=1, max_nb_stays=1):
+    to_keep = stays.groupby('hadm_id').count()[['stay_id']].reset_index()
+    to_keep = to_keep[(to_keep.stay_id >= min_nb_stays) & (to_keep.stay_id <= max_nb_stays)][['hadm_id']]
+    stays = stays.merge(to_keep, how='inner', left_on='hadm_id', right_on='hadm_id')
+    return stays
+
+
+#filter patients younger than 18
+def filter_icustays_on_age(stays, min_age=18, max_age=np.inf):
+    stays = stays[(stays.age >= min_age) & (stays.age <= max_age)]
+    return stays
+
