4fbf4267eea436a8461f573c1ee4b7c929573a7b
Philip <philip.s_08@hotmail.com>
2021-04-12 09:14:06 +0200

    added initial files

---
+++ extract_subjects.py
@@ -0,0 +1,2 @@
+#used to split data by patients, each patient could include multiple ICU stays
+#each patient will have its own serparete folder 
---
+++ mimic4csv.py
@@ -0,0 +1,2 @@
+#helper functions for extract_subjects
+#will have functions to read the csv files and put the data into dataframes
