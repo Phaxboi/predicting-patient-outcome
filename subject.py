342ea958043039712798ebb822c19b02b7ad1c19
Philip <philip.s_08@hotmail.com>
2021-04-15 11:58:03 +0200

    converted 'subjects.py' to work with MIMIC-IV

--- subject.py
+++ subject.py
@@ -5,7 +5,7 @@ import os
 import pandas as pd
 
 
-
+#given a patients folder path, returns the content of its 'stays' file as a dataframe
 def read_stays(subject_path):
     stays = pd.read_csv(os.path.join(subject_path, 'stays.csv'), index_col=None)
     stays.intime = pd.to_datetime(stays.intime)
@@ -17,11 +17,12 @@ def read_stays(subject_path):
     return stays
 
 
+#given a patients folder path, returns the content of its 'diagnosis' file as a dataframe
 def read_diagnoses(subject_path):
     return pd.read_csv(os.path.join(subject_path, 'diagnoses.csv'), index_col=None)
 
 
-
+#given a patients folder path, returns the content of its 'events' file as a dataframe
 def read_events(subject_path, remove_null=True):
     events = pd.read_csv(os.path.join(subject_path, 'events.csv'), index_col=None)
     if remove_null:
@@ -33,7 +34,7 @@ def read_events(subject_path, remove_null=True):
     # events.sort_values(by=['CHARTTIME', 'ITEMID', 'ICUSTAY_ID'], inplace=True)
     return events
 
-
+#given an ID for an icu stay, returns the events for that particular stay
 def get_events_for_stay(events, icustayid, intime=None, outtime=None):
     idx = (events.icustay_id == icustayid)
     if intime is not None and outtime is not None:
@@ -42,7 +43,24 @@ def get_events_for_stay(events, icustayid, intime=None, outtime=None):
     del events['icustay_id']
     return events
 
+#NOTE: not completely sure what this does, think it re-arranges the data as a "time series" with events
+#in cronological order, will have to look into this later
+def convert_events_to_timeseries(events, variable_column='variable', variables=[]):
+    metadata = events[['charttime', 'icustay_id']].sort_values(by=['charttime', 'icustay_id'])\
+                    .drop_duplicates(keep='first').set_index('charttime')
+    timeseries = events[['charttime', variable_column, 'value']]\
+                    .sort_values(by=['charttime', variable_column, 'value'], axis=0)\
+                    .drop_duplicates(subset=['charttime', variable_column], keep='last')
+    timeseries = timeseries.pivot(index='charttime', columns=variable_column, values='value')\
+                    .merge(metadata, left_index=True, right_index=True)\
+                    .sort_index(axis=0).reset_index()
+    for v in variables:
+        if v not in timeseries:
+            timeseries[v] = np.nan
+    return timeseries
 
+#adds elapsed hours as a column to the events dataframe
+#NOTE:also seems to delete charttime, not sure why
 def add_hours_elpased_to_events(events, dt, remove_charttime=True):
     events = events.copy()
     events['hours'] = (events.charttime - dt).apply(lambda s: s / np.timedelta64(1, 's')) / 60./60
@@ -50,18 +68,13 @@ def add_hours_elpased_to_events(events, dt, remove_charttime=True):
         del events['charttime']
     return events
 
+#NOTE: this is also confusing, not exactly sure what it does
+def get_first_valid_from_timeseries(timeseries, variable):
+    if variable in timeseries:
+        idx = timeseries[variable].notnull()
+        if idx.any():
+            loc = np.where(idx)[0][0]
+            return timeseries[variable].iloc[loc]
+    return np.nan
 
 
-def convert_events_to_timeseries(events, variable_column='VARIABLE', variables=[]):
-    metadata = events[['CHARTTIME', 'ICUSTAY_ID']].sort_values(by=['CHARTTIME', 'ICUSTAY_ID'])\
-                    .drop_duplicates(keep='first').set_index('CHARTTIME')
-    timeseries = events[['CHARTTIME', variable_column, 'VALUE']]\
-                    .sort_values(by=['CHARTTIME', variable_column, 'VALUE'], axis=0)\
-                    .drop_duplicates(subset=['CHARTTIME', variable_column], keep='last')
-    timeseries = timeseries.pivot(index='CHARTTIME', columns=variable_column, values='VALUE')\
-                    .merge(metadata, left_index=True, right_index=True)\
-                    .sort_index(axis=0).reset_index()
-    for v in variables:
-        if v not in timeseries:
-            timeseries[v] = np.nan
-    return timeseries
