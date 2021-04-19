d01d5eca019c8efbfbe1322873e19b470a0be571
Philip <philip.s_08@hotmail.com>
2021-04-19 14:37:35 +0200

    bugfixing

--- extract_subjects.py
+++ extract_subjects.py
@@ -10,25 +10,26 @@ from mimic4csv import *
 
 #parsing the command to split the data and describing its parameters
 parser = argparse.ArgumentParser(description='Extract per subject data from the MIMIC-IV dataset')
-parser.add_argument('mimic4_path', type=str, help='Directory containing all MIMIC-IV CSV files.')
-parser.add_argument('output_path', type=str, help='Directory to write the per-subject files to.')
+parser.add_argument('--mimic4_path', type=str, help='Directory containing all MIMIC-IV CSV files.')
+parser.add_argument('--output_path', type=str, help='Directory to write the per-subject files to.')
 args = parser.parse_args()
-
+mimic4_path = args.mimic4_path
+output_path = args.output_path
 
 #create output directory
 try:
-    os.makedirs(args.output_path)
+    os.makedirs(output_path)
 except:
     pass
 
 
 #start reading data from the 'core' folder
-patients = read_patients_table(args.mimic4_path)
-admissions = read_admissions_table(args.mimic4_path)
-transfers = read_transfers_table(args.mimic4_path)
+patients = read_patients_table(mimic4_path)
+admissions = read_admissions_table(mimic4_path)
+transfers = read_transfers_table(mimic4_path)
 
 #read icustays table from the 'ICU' folder
-icustays = read_icustays_table(args.mimic4_path)
+icustays = read_icustays_table(mimic4_path)
 
 #TODO: exclude cases we don't want
 
@@ -53,3 +54,4 @@ patients_info = filter_patients_age(patients_info)
 
 #write a csv
 patients_info.to_csv(os.path.join(output_path, 'stays.csv'), index=False)
+
