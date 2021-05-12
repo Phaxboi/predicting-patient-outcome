cd8d9b92a2f6e1c84868612396fc8d2bb0ef59b7
Philip <philip.s_08@hotmail.com>
2021-05-12 10:29:01 +0200

    tools for plotting PRC and ROC curves

--- generate_in_hospital_mortality_regression_model.py
+++ generate_in_hospital_mortality_regression_model.py
@@ -2,6 +2,12 @@
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import f1_score
+from sklearn.metrics import auc
+from sklearn.model_selection import train_test_split
 
 import os
 import numpy as np
@@ -9,6 +15,7 @@ import argparse
 import pandas as pd
 from tqdm import tqdm
 from metrics import *
+from matplotlib import pyplot
 from prepare_features_mortality_pred import scale_values
 
 import re
@@ -21,39 +28,87 @@ def main():
     args = parser.parse_args()
 
     (data_X, data_Y) = scale_values(args.subjects_root_path)
-    data_X_test = data_X[:150]
-    data_X = data_X[150:]
-    data_Y_test = data_Y[:150]
-    data_Y = data_Y[150:]
-    logreg = LogisticRegression(penalty='l2', C=0.001, random_state=42)
-    logreg.fit(data_X, data_Y)
-    predictions=logreg.predict_proba(data_X)
-    print(sum(data_Y))
-    print(predictions)
-    print(logreg.score(data_X_test, data_Y_test))
+    trainX, testX, trainy, testy = train_test_split(data_X, data_Y, test_size=0.5, random_state=2)
+    testy = np.array(testy)
+    trainy = np.array(trainy)
+    logreg = LogisticRegression(penalty='l2', C=0.01)#, random_state=42
+    logreg.fit(trainX, trainy)
+    print(logreg.score(testX, testy))
 
 
-    #NOTE:rest is compied from becnhmark program
-    result_dir = os.path.join(args.subjects_root_path, 'results')
-    try:
-        os.makedirs(result_dir)
-    except:
-        pass
 
-    file_name = 'result_scuffed_model'
+    ns_probs = [0 for _ in range(len(testy))]
+    lr_probs = logreg.predict_proba(testX)
+    lr_probs = lr_probs[:, 1]
 
-    with open(os.path.join(result_dir, 'train_{}.json'.format(file_name)), 'w') as res_file:
-            ret = print_metrics_binary(data_Y, logreg.predict_proba(data_X))
-            ret = {k : float(v) for k, v in ret.items()}
-            json.dump(ret, res_file)
+    # calculate scores
+    ns_auc = roc_auc_score(testy, ns_probs)
+    lr_auc = roc_auc_score(testy, lr_probs)
+    # summarize scores
+    print('No Skill: ROC AUC=%.3f' % (ns_auc))
+    print('Logistic: ROC AUC=%.3f' % (lr_auc))
+    # calculate roc curves
+    ns_fpr, ns_tpr, _ = roc_curve(testy, ns_probs)
+    lr_fpr, lr_tpr, _ = roc_curve(testy, lr_probs)
+    # plot the roc curve for the model
+    pyplot.plot(ns_fpr, ns_tpr, linestyle='--', label='No Skill')
+    pyplot.plot(lr_fpr, lr_tpr, marker='.', label='Logistic')
+    # axis labels
+    pyplot.xlabel('False Positive Rate')
+    pyplot.ylabel('True Positive Rate')
+    # show the legend
+    pyplot.legend()
+    # show the plot
+    pyplot.show()
+
+    # predict probabilities
+    lr_probs = logreg.predict_proba(testX)
+    # keep probabilities for the positive outcome only
+    lr_probs = lr_probs[:, 1]
+    # predict class values
+    yhat = logreg.predict(testX)
+    lr_precision, lr_recall, _ = precision_recall_curve(testy, lr_probs)
+    lr_f1, lr_auc = f1_score(testy, yhat), auc(lr_recall, lr_precision)
+    # summarize scores
+    print('Logistic: f1=%.3f auc=%.3f' % (lr_f1, lr_auc))
+    # plot the precision-recall curves
+    positive_len = len(testy[testy==1])
+    total_len = len(testy)
+    no_skill = positive_len / total_len 
+    pyplot.plot([0, 1], [no_skill, no_skill], linestyle='--', label='No Skill')
+    pyplot.plot(lr_recall, lr_precision, marker='.', label='Logistic')
+    # axis labels
+    pyplot.xlabel('Recall')
+    pyplot.ylabel('Precision')
+    # show the legend
+    pyplot.legend()
+    # show the plot
+    pyplot.show()
 
 
-    prediction = logreg.predict_proba(data_X_test)[:, 1]
 
-    with open(os.path.join(result_dir, 'test_{}.json'.format(file_name)), 'w') as res_file:
-        ret = print_metrics_binary(data_Y_test, prediction)
-        ret = {k: float(v) for k, v in ret.items()}
-        json.dump(ret, res_file)
+
+    # #NOTE:rest is compied from becnhmark program
+    # result_dir = os.path.join(args.subjects_root_path, 'results')
+    # try:
+    #     os.makedirs(result_dir)
+    # except:
+    #     pass
+
+    # file_name = 'result_scuffed_model'
+
+    # with open(os.path.join(result_dir, 'train_{}.json'.format(file_name)), 'w') as res_file:
+    #         ret = print_metrics_binary(data_Y, logreg.predict_proba(data_X))
+    #         ret = {k : float(v) for k, v in ret.items()}
+    #         json.dump(ret, res_file)
+
+
+    # prediction = logreg.predict_proba(data_X_test)[:, 1]
+
+    # with open(os.path.join(result_dir, 'test_{}.json'.format(file_name)), 'w') as res_file:
+    #     ret = print_metrics_binary(data_Y_test, prediction)
+    #     ret = {k: float(v) for k, v in ret.items()}
+    #     json.dump(ret, res_file)
 
     #save_results(test_names, prediction, test_y, os.path.join(args.output_dir, 'predictions', file_name + '.csv'))
 
--- prepare_features_mortality_pred.py
+++ prepare_features_mortality_pred.py
@@ -23,6 +23,8 @@ def scale_values(subjects_root_path):
     data_Y = []
     mortalities = pd.read_csv(os.path.join(subjects_root_path, 'mortality_summary.csv'))
     mortalities.set_index('stay_id',inplace=True, drop=True)
+
+    #1x15 feature vector
     for root, dirs, files in tqdm(os.walk(subjects_root_path), desc='reading values'):
         for file_name in files:
             if(file_name.startswith('episode') & file_name.endswith('timeseries_48h.csv')):
@@ -42,6 +44,28 @@ def scale_values(subjects_root_path):
                 data_X += [sum_column]
                 data_Y += [mortality]
     
+    # #1x(15x95) feature vector(worse so far)
+    # for root, dirs, files in tqdm(os.walk(subjects_root_path), desc='reading values'):
+    #     for file_name in files:
+    #         if(file_name.startswith('episode') & file_name.endswith('timeseries_48h.csv')):
+    #             episode = pd.read_csv(os.path.join(root, file_name))
+    #             stay_id = re.search('.*_(\d*)_.*', file_name).group(1)
+    #             mortality = mortalities.loc[int(stay_id)].hospital_expire_flag
+    #             #concat every half hour and delete 'hour'
+    #             #also delete 'pH' because there seems to be some huge outliers here
+                
+    #             episode.drop(columns=['hours', 'pH'], axis=1, inplace=True)
+    #             columns = []
+    #             for index, row in episode.iterrows():
+    #                columns += row.tolist()
+    #             #columns = [row.tolist() for index, row in episode.iterrows()]
+
+    #             #store the imputed values to use with scaler
+    #             data_X += [columns]
+    #             data_Y += [mortality]
+
+
+
     scaler = StandardScaler()
     scaler.fit(data_X)
     data_X_standardized = scaler.transform(data_X)
@@ -53,31 +77,7 @@ def scale_values(subjects_root_path):
     # plt.hist(debug_lista_filtered,density=True, bins=20)
     # plt.show()
 
-    # data_X_standardized = scaler.transform(data_X)
-    # debug_lista = [lista[2] for lista in data_X_standardized]
-    # #debug_lista_filtered = [item for item in debug_lista if item <300]
-    # plt.hist(debug_lista, density=True, bins=20)
-    # plt.show()
 
-    # #open each file and normalizes the data to have zero mean and unit variance
-    # for root, dirs, files in os.walk(subjects_root_path):
-    #     episode_counter = 0
-    #     for file_name in files:
-    #         if(file_name.startswith('episode') & file_name.endswith('timeseries_48h.csv')):
-    #             episode_counter += 1
-    #             episode = pd.read_csv(os.path.join(root, file_name))
-                
-    #             episode_normalized = np.array(scaler.transform(episode), dtype=np.float32)
-    #             episode_normalized = pd.DataFrame(episode_normalized)
-    #             episode_normalized.columns = column_names
-
-    #             #for debugging
-    #             values = episode_normalized.values.tolist()
-    #             data_X = data_X + values
- 
-    #             subj_id = re.search('.*_(\d*)_.*', file_name).group(1)
-    #             file_name = 'episode' + str(episode_counter) + '_' + str(subj_id) + '_timeseries_48h_normalized.csv'
-    #             episode_normalized.to_csv(os.path.join(root, file_name), index=False)
 
 #NOTE: COPIED FROM BENCHMARK PROGRAM
 # def save_results(names, pred, y_true, path):
