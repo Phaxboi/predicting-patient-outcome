3659198071896db55ed1dc7744216c1eafa0df49
Philip <philip.s_08@hotmail.com>
2021-09-21 09:45:54 +0200

    fixed word2vec files

--- word_embeddings/preprocessing.py
+++ word_embeddings/preprocessing.py
@@ -12,6 +12,7 @@ doc = open("word_embeddings/data/" + options.target_file_name, encoding="utf16")
 lines = doc.readlines()
 
 processed = [gensim.utils.simple_preprocess(line) for line in lines]
+
 # # remove words that appear only twice
 # frequency = defaultdict(int)
 # for text in processed:
@@ -19,6 +20,13 @@ processed = [gensim.utils.simple_preprocess(line) for line in lines]
 #         frequency[token] += 1
 
 # processed = [[token for token in text if frequency[token] > 2]for text in processed]
+
+
+# # Create a set of frequent words
+# stoplist = set('for a of the and to in'.split(' '))
+# # filter out stopwords
+# processed = [[word for word in document if word not in stoplist] for document in processed]
+
 
 #open file to write processed corpus to
 corpus_file = open("word_embeddings/corpus/" + options.target_file_name, "w", encoding="utf16")
--- word_embeddings/word2vec.py
+++ word_embeddings/word2vec.py
@@ -4,6 +4,7 @@ from smart_open import open
 import gensim
 from collections import defaultdict
 import time
+from gensim.models import Phrases
 
 
 from sklearn.decomposition import IncrementalPCA    # inital reduction
@@ -22,7 +23,7 @@ class MySentences(object):
  
     def __iter__(self):
         for line in open("corpus/" + self.fname + ".txt", encoding="utf16"):
-            yield [line]
+            yield line.split()
  
 sentences = MySentences(options.target_file_name) # a memory-friendly iterator
 
@@ -36,3 +37,19 @@ end = time.time()
 print("time to train model: " + str((end - start)))
 
 model.save("models/" + options.output_name + "_model")
+
+##Code to generate bi/tri-grams
+# bigram = Phrases(sentences, min_count=5)
+# trigram = Phrases(bigram[sentences], min_count=5)
+# print(len(model.wv.index_to_key))
+# print(len(bigram.vocab))
+# print(len(trigram.vocab))
+
+
+# start = time.time()
+# model = gensim.models.Word2Vec(trigram[sentences],vector_size=100, min_count=5, workers=4)
+# end = time.time()
+# print("time to train trigram model: " + str((end - start)))
+# print(len(model.wv.index_to_key))
+
+# model.save("models/" + options.output_name + "trigram_model")
